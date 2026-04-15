24ae8403c02d29c4a2b15c533ce066e781c5c796
jtuomi <jtuomi@student.hive.fi>
2026-04-15 20:33:40 +0300

    fix: compiler errors

--- src/app/keybind.rs
+++ src/app/keybind.rs
@@ -11,37 +11,41 @@ use cosmic::iced::keyboard::key::Named;
 ///
 /// | Character | Binding |
 /// |-----------|---------|
-/// | F1 |			=> about |
-/// | Enter |		=> take a picture or start/stop recording |
+/// | F1 |          => about |
+/// | Enter |       => take a picture or start/stop recording |
+/// | a |           => toggles auto focus |
+/// | c |           => toggles color picker |
+/// | e |           => toggles exposure picker |
+/// | f |           => toggles flash |
+/// | g |           => opens gallery |
+/// | n |           => next mode |
+/// | m |           => toggle audio recording or mute/unmute |
+/// | p |           => toggle motor picker |
+/// | q |           => toggle QR detection |
+/// | r |           => toggle save burst RAW |
+/// | s |           => switches camera |
+/// | t |           => toggles timelapse |
+/// | u |           => toggles theatre UI |
+/// | v |           => toggles virtual camera |
+/// |   |           => toggles play/pause |
+/// | Ctrl + a |    => cycles aspect ratio |
+/// | Ctrl + f |    => toggles format picker |
+/// | Ctrl + q |    => currently noop //TODO: close app |
+/// | Ctrl + r |    => reset settings |
+/// | Ctrl + t |    => toggles theatre mode |
+/// | Ctrl + + |    => zoom in |
+/// | Ctrl + - |    => zoom out |
+/// | Ctrl + 0 |    => reset zoom |
+/// | Ctrl + , |    => opens/closes settings |
 /// | Ctrl + Enter |=> start recording after delay |
-/// | a |			=> toggles auto focus |
-/// | c |			=> toggles color picker |
-/// | e |			=> toggles exposure picker |
-/// | f |			=> toggles flash |
-/// | g |			=> opens gallery |
-/// | n |			=> next mode |
-/// | m |			=> toggle audio recording or mute/unmute |
-/// | p |			=> toggle motor picker |
-/// | q |			=> toggle QR detection |
-/// | r |			=> toggle save burst RAW |
-/// | s |			=> switches camera |
-/// | t |			=> toggles timelapse |
-/// | u |			=> toggles theatre UI |
-/// | v |			=> toggles virtual camera |
-/// |   |			=> toggles play/pause |
-/// | Ctrl + a |	=> cycles aspect ratio |
-/// | Ctrl + f |	=> toggles format picker |
-/// | Ctrl + q |	=> currently noop //TODO: close app |
-/// | Ctrl + r |	=> reset settings |
-/// | Ctrl + t |	=> toggles theatre mode |
-/// | Ctrl + + |	=> zoom in |
-/// | Ctrl + - |	=> zoom out |
-/// | Ctrl + 0 |	=> reset zoom |
-/// | Ctrl + , |	=> opens/closes settings |
 ///
 /// **Returns** a subscription to key events mapped similarly as GNOME Camera's for now
-pub fn key_subscription(mode: CameraMode) -> Subscription<Message> {
-    cosmic::iced::event::listen_raw(|event, _status, _window| {
+pub fn key_subscription() -> Subscription<Message> {
+    fn event_to_functionality(
+        event: cosmic::iced::Event,
+        _status: cosmic::iced::event::Status,
+        _window: cosmic::iced::window::Id,
+    ) -> Option<Message> {
         let Event::Keyboard(keyboard::Event::KeyPressed { key, modifiers, .. }) = event else {
             return None;
         };
@@ -52,39 +56,37 @@ pub fn key_subscription(mode: CameraMode) -> Subscription<Message> {
             {
                 Some(Message::ToggleContextPage(ContextPage::About))
             }
+            // TODO: couldn't capture mode, need to think about it
+            // Named::Key(Named::Enter)
+            //     if !modifiers.control()
+            //         && !modifiers.logo()
+            //         && !modifiers.alt()
+            //         =>
+            // {
+            //     Some(Message::ToggleRecording())
+            // }
             Named::Key(Named::Enter)
-                if !modifiers.control()
-                    && !modifiers.logo()
-                    && !modifiers.alt()
-                    && mode == CameraMode::Video =>
+                if !modifiers.control() && !modifiers.logo() && !modifiers.alt() =>
             {
-                Some(Message::ToggleRecording())
-            }
-            Named::Key(Named::Enter)
-                if !modifiers.control()
-                    && !modifiers.logo()
-                    && !modifiers.alt()
-                    && mode == CameraMode::Camera =>
-            {
-                Some(Message::Capture())
+                Some(Message::Capture)
             }
             Named::Key(Named::Enter)
                 if modifiers.control() && !modifiers.logo() && !modifiers.alt() =>
             {
-                Some(Message::StartRecordingAfterDelay())
+                Some(Message::StartRecordingAfterDelay)
             }
 
             Key::Character(c) if modifiers.control() && !modifiers.logo() && !modifiers.alt() => {
                 match c.as_str() {
-                    "a" => Some(Message::CyclePhotoAspectRatio()),
-                    "f" => Some(Message::ToggleFormatPicker()),
+                    "a" => Some(Message::CyclePhotoAspectRatio),
+                    "f" => Some(Message::ToggleFormatPicker),
                     "q" => Some(Message::Noop),
-                    "r" => Some(Message::ResetAllSettings()),
-                    "t" => Some(Message::ToggleTheatherMode()),
-                    "+" => Some(Message::ZoomIn()),
-                    "-" => Some(Message::ZoomOut()),
-                    " " => Some(Message::AbortPhotoTimer()),
-                    "0" => Some(Message::ResetZoom()),
+                    "r" => Some(Message::ResetAllSettings),
+                    "t" => Some(Message::ToggleTheatherMode),
+                    "+" => Some(Message::ZoomIn),
+                    "-" => Some(Message::ZoomOut),
+                    " " => Some(Message::AbortPhotoTimer),
+                    "0" => Some(Message::ResetZoom),
                     "," => Some(Message::ToggleContextPage(ContextPage::Settings)),
                     _ => None,
                 }
@@ -92,24 +94,25 @@ pub fn key_subscription(mode: CameraMode) -> Subscription<Message> {
 
             Key::Character(c) if !modifiers.control() && !modifiers.logo() && !modifiers.alt() => {
                 match c.as_str() {
-                    "a" => Some(Message::ToggleFocusAuto()),
-                    "c" => Some(Message::ToggleColorPicker()),
-                    "e" => Some(Message::ToggleExposurePicker()),
-                    "f" => Some(Message::ToggleFlash()),
-                    "g" => Some(Message::OpenGallery()),
-                    "n" => Some(Message::NextMode()),
-                    "m" => Some(Message::ToggleRecordAudio()),
-                    "p" => Some(Message::ToggleMotorPicker()),
-                    "q" => Some(Message::ToggleQrDetection()),
-                    "r" => Some(Message::ToggleSaveBurstRaw()),
-                    "s" => Some(Message::SwitchCamera()),
-                    "t" => Some(Message::ToggleTimelapse()),
-                    "u" => Some(Message::TheatreToggleUI()),
-                    "v" => Some(Message::ToggleVirtualCamera()),
-                    " " => Some(Message::ToggleVideoPlayPause()),
+                    "a" => Some(Message::ToggleFocusAuto),
+                    "c" => Some(Message::ToggleColorPicker),
+                    "e" => Some(Message::ToggleExposurePicker),
+                    "f" => Some(Message::ToggleFlash),
+                    "g" => Some(Message::OpenGallery),
+                    "n" => Some(Message::NextMode),
+                    "m" => Some(Message::ToggleRecordAudio),
+                    "p" => Some(Message::ToggleMotorPicker),
+                    "q" => Some(Message::ToggleQrDetection),
+                    "r" => Some(Message::ToggleSaveBurstRaw),
+                    "s" => Some(Message::SwitchCamera),
+                    "t" => Some(Message::ToggleTimelapse),
+                    "u" => Some(Message::TheatreToggleUI),
+                    "v" => Some(Message::ToggleVirtualCamera),
+                    " " => Some(Message::ToggleVideoPlayPause),
                     _ => None,
                 }
             }
         }
-    })
+    }
+    cosmic::iced::event::listen_raw(event_to_functionality)
 }
